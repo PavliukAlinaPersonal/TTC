bf0d9031f4af1bbc794be3e7d649f28c6892d4b6
PavliukAlinaPersonal <pavliuk.alina.personal@gmail.com>
2021-07-09 01:59:32 +0300

    kk

--- TTC/UnitTest1.cs
+++ TTC/UnitTest1.cs
@@ -1,4 +1,6 @@
 using NUnit.Framework;
+using System;
+using System.IO;
 
 namespace TTC
 {
@@ -12,8 +14,20 @@ namespace TTC
         [Test]
         public void Test1()
         {
-            Assert.Pass();
-            Assert.Pass();
+            // Create a string array with the lines of text
+            string[] lines = { "First line", "Second line", "Third line" };
+
+            // Set a variable to the Documents path.
+            string docPath =
+              Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            // Write the string array to a new file named "WriteLines.txt".
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "WriteLines.txt")))
+            {
+                foreach (string line in lines)
+                    outputFile.WriteLine(line);
+            }
+            Assert.Pass();//
         }
     }
 }
